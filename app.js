e2064e78d2157f42d77b2392bf08fc3a01376f71
kasreder <[kasreder@naver.com]>
2018-05-22 23:51:28 +0900

    원하는 아이디로 검색하여 글목록, 일주일간 수익

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ steem.api.getDiscussionsByBlog({"tag": "kview", "limit":3, "start_author":"leeso
   console.log(err, result);
 });*/
 
-steem.api.getDiscussionsByTrending({"tag":"kr", "limit":10,"author":"smartbear"}, function(err, result){
-  console.log(err, result);
-})
+steem.api.getDiscussionsByAuthorBeforeDate('lovehm1223','','2018-05-22T09:00:00',1, function(err, result){
+  if (err){
+    console.log(err)
+  }else{
+    for (i = 0; i < result.length; i++){
+      title = result[i].title
+      pending_payout_value1 = result[i].pending_payout_value
+      console.log(title, pending_payout_value1)
+      /*제목 일주일치보팅받은스달 pending_payout_value:일주일간 수익코인 */
+  }
+}
+});
